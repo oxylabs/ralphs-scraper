97547067aba5ff61a3426d9afd32adbc82402128
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:52:23 +0200

    Adding code examples/ralphs_curl.sh

---
+++ code examples/ralphs_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.ralphs.com/pl/tvs-home-theater/29001?pzn=relevance"}'
